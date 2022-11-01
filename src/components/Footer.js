26f34841d0da0c8f334a23b3132944ebc91f0268
Quentin <alixetquentin@quentin-roman.lan>
2022-11-01 17:19:34 +0100

    modif

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <div>
+        <span>Made with</span>
+        <a target="blank" href="https://fr.reactjs.org/">
+          React
+        </a>
+        <span>at </span>
+        <a target="blank" href="https://www.lereacteur.io/">
+          Le Reacteur
+        </a>
+        <span> by </span>
+        Quentin
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
